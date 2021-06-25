84fed44b3cbb05a25175fcca19af478aa126a1b3
Lan <15669147150@163.com>
2021-06-25 16:54:49 +0800

    增加正则判断

--- src/page/index.vue
+++ src/page/index.vue
@@ -11,6 +11,34 @@ export default {
   name: 'Home',
   components: {
     Header
+  },
+  mounted() {
+    let str = 'A||B||Y&&U'
+    console.log(this.regulars(str))
+  },
+  methods:{
+    regulars(str) {
+      var patt = /^[A-Z]{1}([&&]|[||]){2}/;
+      var patt1 = /^[A-Z]{1}$/;
+      var three = 3;
+      for (var i = 0, l = str.length; i < l/three; i++) {
+        let a = str.slice(three*i, three*(i+1));
+        if (a.length >= 3) {
+          if(!patt.test(a)) {
+            return false
+          }
+        } else {
+          if(!patt1.test(a)) {
+            return false
+          }
+        }
+      }
+      const tests = str.charAt(str.length-1)
+      if(!patt1.test(tests)) {
+        return false
+      }
+      return true
+    }
   }
 }
 </script>
